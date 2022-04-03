a319154b94edb75aee399e88cd2a38f53df5526d
Jan Löhr <janloehr@live.com>
2022-04-03 18:48:19 +0200

    Add project files.

---
+++ MauiApp1/ListItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiApp1
+{
+    internal class ListItemViewModel : ExtendedBindableObject
+    {
+        public string ButtonLabel { get; set; }
+
+        private int _clicksReceived = 0;
+        public int ClicksReceived { get { return _clicksReceived; } set { _clicksReceived = value; RaisePropertyChanged(() => ClicksReceived); } }
+        public ICommand ActionClickedCommand { get; set; }
+
+        public ListItemViewModel(string label)
+        {
+            ButtonLabel = label;
+            ActionClickedCommand = new Command(OnActionClicked);
+        }
+
+        private void OnActionClicked(object obj)
+        {
+            ClicksReceived++;
+        }
+    }
+}
---
+++ MauiApp1/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MauiApp1
+{
+    internal class MainPageViewModel
+    {
+        public ObservableCollection<ListItemViewModel> ListItems { get; set; } = new ObservableCollection<ListItemViewModel>();
+
+        public MainPageViewModel()
+        {
+            ListItems.Add(new ListItemViewModel("One"));
+            ListItems.Add(new ListItemViewModel("Two"));
+            ListItems.Add(new ListItemViewModel("Three"));
+        }
+    }
+}
